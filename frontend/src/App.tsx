f1bf9869e70dd9a79c1d214208eeda8758135bc9
Januda Bethmin <desilvabethmin@gmail.com>
2024-05-06 19:42:13 +0530

    number of clicks button demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
-  // Here we wan't something to save the current state of the application
+  // Here we want something to save the current state of the application
   // We need to notify react to reach out to UI to display the new values
   // We can create a state variable to store the current state of the application
   // clickCount is the state variable and setClickCount is the function to update the state
+  // Because we initialized the state with 0, clickCount is 0
   const [clickCount, setClickCount] = useState(0);
 
+  // The return statement returns the actual UI element
   return (
     <div className="App">
       <header className="App-header">
@@ -18,14 +21,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Button is a react-bootstrap component */}
+        {/* We can pass the clickCount variable to the Button component */}
+        {/* We can set a onClick event listener to the button and call the setClickCount function to update the state */}
+        {/* The clickCount will be incremented once the button is clicked*/}
+        <Button onClick={() => setClickCount(clickCount + 1)}>
+          Clicked {clickCount} times
+        </Button>
       </header>
     </div>
   );
