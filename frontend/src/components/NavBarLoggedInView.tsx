e606d1648c7557fce79b0641ada661a262bf7940
Januda Bethmin <desilvabethmin@gmail.com>
2024-06-24 15:53:30 +0530

    NavBarLoggedInView created

--- frontend/src/components/NavBarLoggedInView.tsx
+++ frontend/src/components/NavBarLoggedInView.tsx
@@ -1,5 +1,6 @@
 // Creating a functional component that will display the navbar when the user is logged in
 
+import { Button, Navbar } from "react-bootstrap";
 import { User } from "../models/user";
 // Importing all from NotesApi
 import * as NotesApi from "../network/notesApi";
@@ -14,10 +15,10 @@ interface NavBarLoggedInViewProps {
 }
 
 // Creating the component
-const NavBarLoggedInView = (
+const NavBarLoggedInView = ({
   user,
-  onLogoutSuccessful: NavBarLoggedInViewProps
-) => {
+  onLogoutSuccessful,
+}: NavBarLoggedInViewProps) => {
   // Now we need a function to logging out that we can declare directly in the component
   // We can declare it here because we are not going to use it anywhere else
   async function logout() {
@@ -39,7 +40,14 @@ const NavBarLoggedInView = (
     // We can put this into a div but we can only need to render them in the navbar
     // So we can simply use fragments.
     // Fragments allows us to use two or more element where only one is expected
-    <></>
+    <>
+      {/* We are going to display the username of the loggedIn user using a Navbar.Text component */}
+      <Navbar.Text className="mr-2">Welcome {user.username}</Navbar.Text>
+      {/* We are going to add a logout button */}
+      <Button variant="outline-light" onClick={logout}>
+        Logout
+      </Button>
+    </>
   );
 };
 
